9c7243fea5aab6498f76b64354bd94377eeb333b
Martin-koder1 <martinmartinkoder@gmail.com>
2024-11-27 11:55:45 +0100

    try again

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
 // TASK:
-// 1. Create a constant variable that retrieves the HTML element with ID inputData.
-// and log the variable to the console to verify correct resrival.
+// 1. Create a constant variable that retrives the HTML element with ID inputData
+// and log the variable to the console to verify correct retrival.
 
-// 5. add the method .value to retrieve the value from the inut of the element itself.
-
-const inputData = document.querySelector('#inputData').value;
+const inputData = document.querySelector("#inputData");
 console.log(inputData);
 
-// 2. Create a constant variable thatretrieves the HTML element with ID submitData
-// and log the variable to the console to verify correct retrievel.
+// 2. Create a constant variable that retrives the HTML element with ID submitData
+// and log the variable to the console to verify correct retrival.
 
-const submitData = document.querySelector('#submitData')
+const submitData = document.querySelector("#submitData");
 console.log(submitData);
 
-// 3. create a function with name displayData
-// and log a string saying "in working" to the console log
+// 3. Create a function with name displayData
+// and log a string saying "Im working" to the console
 
-// 6. create a variable called inputDataValue that uses the variable inputData with the value method.
-// add a log logging out the variable input inside of the function. 
+// 6. Create a variable called inputDataValue that uses the variable inputData with the .value method.
+// Add a log logging out the variable inputDataValue inside of the function.
+
+// 7. create a constant variable called displayedData that retrieves the HTML element with the ID displayed data inside the function
+// And log out the variable to verify that it is correct.
+
+// 8. Create a constant variable called listData with the .createElement method inside the function.
+// and log out the newly created variable to verify that it is made.
+
+// 9. Give the variable listData content from inputDataValue with the .textcontent method
+
+//10. Add he new element listData to the element displayedData with the .appendChild() method.
 
 function displayData() {
-    console.log("I'm working");
-
+    console.log("Im working");
+    
     const inputDataValue = inputData.value;
-    console.log(inputDataValue)
+    console.log(inputDataValue);
+
+    const displayedData = document.querySelector('#displayedData');
+    console.log(displayedData);
+
+    const listData = document.createElement("li");
+    console.log(listData);
+
+    listData.textContent = inputDataValue;
+    
+    displayedData.appendChild(listData);
 };
 
-// 4. Use the variable submitData and add an event listner, listening for a click and activating function Displaydata.
+// 4. Use the variable submitData and add an event listener, listening for a click and activating the function displayData
 
 submitData.addEventListener("click", displayData);
